db515a1045cfe21b3bc06c83ce14a59ff24f661d
Himank Singh <Himank.Singh@0101digitall.com>
2026-05-16 20:54:17 +0530

    added intial config

---
+++ client/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
+import svgr from "vite-plugin-svgr";
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      svgr({
+        include: "**/*.svg?react",
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    server: {
+      port: Number(env.VITE_APP_PORT) || 5173,
+    },
+  };
+});
